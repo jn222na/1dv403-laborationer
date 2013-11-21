20ba1052a7c0c896ffbaa41992f844d9f228e6f4
OpenShift guest <jn222na@student.lnu.se>
2013-11-21 09:26:12 -0500

    4 klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,31 +1,44 @@
 "use strict";
 
 
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
 var makePerson = function(persArr) {
-    
+    /*
     data.sort(function(a, b){
   return a.age - b.age;
 });
-var strang;
-
+*/
 var result = {};
-result.names = persArr.map(function (pers) {
-    pers.age;
-    console.log(pers.age);
-    return pers.name;
-}).join(", ");
+var sum =0;
+// Skapar objektet result
 
+/* Skapar en array för namnen */
+var name = [persArr[0].name,persArr[1].name,persArr[2].name];
+name.sort(function(a,b){
+        return a.localeCompare(b);
+});
+// Lägger till arrayen name till result.names
+result.names = name.join(", ");
 
-console.log(result)
-return [result];
+/*Skapar en array för åldrarna */
+var age = [persArr[0].age,persArr[1].age,persArr[2].age];
+//Sorterar åldrarna
+age.sort();
+//Fixar lägsta, högsta.
+result.minAge = Math.min.apply(Math, age);
+result.maxAge = Math.max.apply(Math, age);
+//Gör en for loop som tar alla väden i arrayen och summerar dom till variabeln sum.
+for (var i = 0; i < persArr.length; i++) {
+    sum += parseInt(persArr[i].age);
+}
+//Fixar medelvärdet
+result.averageAge = Math.ceil((sum/persArr.length));
+console.log(result);
+return result;
 };
 
 
-var result =  makePerson(data);
 
 
 
-var arrayfunk = function(name, age){
-};
 

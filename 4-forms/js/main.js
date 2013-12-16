71cc8aeae69b7e7d99e2895afbe926b9b8d2b819
OpenShift guest <jn222na@student.lnu.se>
2013-12-16 11:33:09 -0500

    4_3

--- 4-forms/js/main.js
+++ 4-forms/js/main.js
@@ -2,47 +2,122 @@
 
 var validation = {
 
+	init : function(fornameFocus) {
+		console.log("function: init");
+		//Skicka med fornameFocus funkar inte varför ?
+		var reason = "";
+		var button = document.querySelector("#singlebutton");
+		console.log(button);
+		validation.forname();
+		//validation.email();
+	},
+	forname : function() {
+		console.log("function: Forname");
+		//Var
+		var allowedName = /^[\ws*$åäöÅÄÖ]/;
+		//a-z A-Z 0-9 _ [åäö][ÅÄÖ] blanksteg
+		//Förnamn
 
-			init: function(){
-				console.log("function: init");
-				var reason = "";
-				var button = document.querySelector("#singlebutton");
-				console.log(button);
-				button.onclick =	validation.postalCode;
-				reason += validation.postalCode;
-				validation.email();
-			},
+		var fornameFocus = document.querySelector("#Fornamn");
 
-			 postalCode: function(){
-			 	console.log("function: Postalcode");
-			 	//Var
-			 	var error;
-			 	var allowedPostal = /^[\wåäöÅÄÖ]/;
-			 	//a-z A-Z 0-9 _ [åäö][ÅÄÖ] allowed  
-			 	var whiteSpace = /^\s*$/;
-			 	 //Kollar blanksteg samt,
-				var postalCodeId = document.querySelector("#Fornamn").value;
-				console.log(postalCodeId);
-				
-				if(whiteSpace.test(postalCodeId)){
-					alert("Blank eller mellanslag innan namn");
-				}
-				if(allowedPostal.test(postalCodeId)){
-				}
-				else{
-					alert("Fel tecken endast a-z,A-Z, 0-9, tillåtna");
-				}
-			},
-			
-			 email: function(){
-				console.log("function: email");
-				var textvaliEmail ="";
-			},
-			
+		//Efternamn
+		var lastnameId = document.querySelector("#Efternamn").value;
+		var lastnameFocus = document.querySelector("#Efternamn");
+		lastnameFocus.onblur = function() {
+			var fornameId = document.querySelector("#Fornamn").value;
+			if(allowedName.test(fornameId)) {
 
+				//true
+			} else {
+				//false
+				if( lastnameId = /^[\s]/) {
+					console.log("WHITESPACE");
+					alert("Fel \nKolla igenom  ditt Förnamn följande är tillåtet\na-Ö\n0-9\nTom");
+					fornameFocus.focus();
+					validation.forname();
+				}
+
+			}
+			validation.efternamn();
+		};
+		//Slut lastnameId.onfocus
+	},
+	efternamn : function() {
+		console.log("function: efternamn");
+		var allowedName = /^[\ws*$åäöÅÄÖ]/;
+		//Efternamn
+
+		var lastnameFocus = document.querySelector("#Efternamn");
+		//Postnummer
+		var postnumberId = document.querySelector("#Postnummer").value;
+		var postnumberFocus = document.querySelector("#Postnummer");
+		postnumberFocus.onblur = function() {
+			var lastnameId = document.querySelector("#Efternamn").value;
+			if(allowedName.test(lastnameId)) {
+				//true
+			} else {
+				//false
+				if( postnumberId = /^[\s]/) {
+					console.log("WHITESPACE");
+					alert("Fel \nKolla igenom ditt Efternamn följande är tillåtet\na-Ö\n0-9\nTom");
+					lastnameFocus.focus();
+					validation.efternamn();
+				}
+
+			}
+			validation.postnumber();
+		};
+		//Slut lastnameId.onfocus
+	},
+	postnumber : function() {
+		console.log("function: postnummer");
+		var allowedName = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		//Efternamn
+		var postnumberFocus = document.querySelector("#Postnummer");
+		//Postnummer
+		var epostId = document.querySelector("#Epost").value;
+		var epostFocus = document.querySelector("#Epost");
+		epostFocus.onblur = function() {
+			var postnumberId = document.querySelector("#Postnummer").value;
+			if(allowedName.test(postnumberId)) {
+				//true
+			} else {
+				//false
+				if( epostId = /^[\s]/) {
+					console.log("WHITESPACE");
+					alert("Fel \nKolla igenom ditt Postnummer följande är tillåtet\na-Ö\n0-9\nTom");
+					postnumberFocus.focus();
+					validation.postnumber();
+				}
+
+			}
+			validation.epost();
+		};
+		//Slut lastnameId.onfocus
+	},
+	epost : function() {
+		var allowedName = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+		//Efternamn
+
+		var epostFocus = document.querySelector("#Epost");
+		//Postnummer
+		var epostId = document.querySelector("#Epost").value;
+		var epostFocus = document.querySelector("#Epost");
+		epostFocus.onblur = function() {
+			var epostId = document.querySelector("#Epost").value;
+			if(allowedName.test(epostId)) {
+				//true
+			} else {
+				//false
+				if( epostId = /^[\s]/) {
+					console.log("WHITESPACE");
+					alert("Fel \nKolla igenom din email följande format är tillåtet\nxxx@xxx.xxx\nxxx@xxx.xx");
+					epostFocus.focus();
+					validation.epost();
+				}
+				validation.epost();
+			}//Slut lastnameId.onfocus
+		};
+	},
 };
-window.onload = validation.init;//Slut onload
-
-			
-
-
+window.onload = validation.init;
